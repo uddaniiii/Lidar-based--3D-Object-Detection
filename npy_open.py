5571998dfeba48cf263237f3de1266e03d23b346
“daku <knzss3382@gmail.com>
2024-09-25 22:37:47 +0900

    test

--- npy_open.py
+++ npy_open.py
@@ -9,5 +9,4 @@ pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(point_cloud[:, :3])  # 첫 세 열은 x, y, z 좌표
 
 # 포인트 클라우드 시각화
-o3d.visualization.draw_geometries([pcd])
-
+o3d.visualization.draw_geometries([pcd])    
